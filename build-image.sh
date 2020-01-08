cf308f27d78026e93551d17236ace1f55b67874c
Mark Alston <malston@pivotal.io>
2020-01-08 10:33:59 -0700

    Separate build image for docker versus harbor

--- build-image.sh
+++ build-image.sh
@@ -4,22 +4,11 @@ set -eo pipefail
 
 docker_image_name="${1:-"malston/alert-webhook"}"
 docker_image_tag="${2:-"0.1.1"}"
-project="${3:-"prometheus"}"
 
 make build-linux
 
 docker build -t "${docker_image_name}:${docker_image_tag}" .
 
-if [[ -z "${HARBOR_ADMIN_PASSWORD}" ]]; then
-  echo "Enter the password for the harbor administrator account: "
-  read -rs HARBOR_ADMIN_PASSWORD
-fi
-
-if [[ -z "${HARBOR_URL}" ]]; then
-  echo "Enter the dns hostname for harbor: (e.g. harbor.example.com)"
-  read -r HARBOR_URL
-fi
-
-docker login "https://${HARBOR_URL}" --username admin --password "${HARBOR_ADMIN_PASSWORD}"
-docker tag "${docker_image_name}:${docker_image_tag}" "${HARBOR_URL}/${project}/${docker_image_name}:${docker_image_tag}"
-docker push "${HARBOR_URL}/${project}/${docker_image_name}:${docker_image_tag}"
+docker login
+docker tag "${docker_image_name}:${docker_image_tag}" "${docker_image_name}:${docker_image_tag}"
+docker push "${docker_image_name}:${docker_image_tag}"
