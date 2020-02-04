7b948400c7495113841a53252b35d27483af074f
Mark Alston <malston@pivotal.io>
2020-02-04 10:25:34 -0700

    Update install script to recreate token

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl delete configmap am-webhook-config --ignore-not-found
-kubectl create configmap am-webhook-config --from-file=config/ --namespace=monitoring
-
-kubectl delete deploy alert-webhook --ignore-not-found
-kubectl create -f deployment.yaml
